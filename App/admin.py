be2e501849ef9101c26c75b3238cac11da82e862
Ivan Villarrubia <ivan@toximosi.es>
2022-04-10 17:30:10 +0200

    preparacion de la parte de administracion usuario:admin pass:1234

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,10 @@
+#importaciones----------------------------
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+#Code---------------------------------
+admin.site.register(Cursos)
+admin.site.register(Estudiantes)
+admin.site.register(Profesores)
+admin.site.register(Entregables)
