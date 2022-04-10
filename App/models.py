3fcf1c50c9b5b4650a6c9f014e04ec15c0bdd084
Ivan Villarrubia <ivan@toximosi.es>
2022-04-10 15:30:20 +0200

    preparación de modelos bd

--- App/models.py
+++ App/models.py
@@ -3,5 +3,25 @@ from turtle import title
 from django.db import models
 
 # Create your models here.
-class Model(models.Model):
-    title = models.CharField(max_length=100)
+class Profesores(models.Model):
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=20)
+    curso = models.CharField(max_length=20)
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=20)
+    curso = models.IntegerField()
+class Cursos(models.Model):
+    nombre = models.CharField(max_length=50)
+    entregables = models.CharField(max_length=10)
+    FechaDeInicio = models.DateField()
+    FechaDeFin = models.DateField()
+    Horario = models.TimeField()
+
+class Entregables(models.Model):
+    nombre = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=20)
+    FechaEntrega = models.DateField()
+    Nota = models.IntegerField()
+
